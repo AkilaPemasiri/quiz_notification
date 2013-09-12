30813fb862ac00bea935d998daf6a29bdf3b6855
100374A <akilasbv@gmail.com>
2013-09-12 11:22:18 +0530

    commit 3 : version 2013091130
    final message print

--- block_quiz_notification.php
+++ block_quiz_notification.php
@@ -1,36 +1,97 @@
 <?php
-class block_quiz_notification extends block_base{
+
+class block_quiz_notification extends block_base {
+
     public function init() {
         $this->title = get_string('quiz_notification', 'block_quiz_notification');
-        
     }
+
     public function get_content() {
-       if ($this->content !== null) {
-    return $this->content;
-  }
- 
- $this->content         =  new stdClass;
-    $this->content->text   = 'The content of our SimpleHTML block!';
-    $this->content->footer = 'Footer here...';
- 
-    return $this->content;
-    }
-    public function cron(){
-        $fp = fopen("my.txt", "wb");
-        
-        if($fp == FALSE){
-           echo 'Hey, i didnt write to file';    
-        }else {
-            fwrite($fp, "aa");
-            fclose($fp);
-             echo 'Hey, i wrote to file';
-            
+        if ($this->content !== null) {
+            return $this->content;
         }
-            
-         echo 'Hey, my cron script is running';
-        
+
+        $this->content = new stdClass;
+        $this->content->text = 'The content of our SimpleHTML block!';
+        $this->content->footer = 'Footer here...';
+
+        return $this->content;
     }
+
+    public function cron() { // cron exe seems to be not working
+        global $DB;
+        $now = time();
+        
+        //  echo $now;
+      $instances = $DB->get_records_sql('select * from mdl_quiz');
+        // $result = mysql_query("select quiz.id,course,name,timeopen from quiz where cou");
+        foreach ($instances as $id) {
+             $fp = fopen("C:\Users\akila\Desktop\myTextttt.txt","a");
+
+
+            if($fp==false){
+                echo 'oh fp is false'; 
+            }
+            else{
+                fwrite($fp, "akila");
+                fclose($fp);
+            }
+            
+            
+            //testing
+        }
+            $starttime1 = $id->timeopen;
+            $starttime = $starttime1 - 19800;
+            $delay = $starttime - $now;
+
+            $gap = $delay / 60;
+            if (0 <= $gap && $gap <= 1) {
+                 $courseid = $id->course;
+                 create_quize_notification($courseid,$starttime);
+//                $courseid = $id->course;
+//                $result = mysql_query("select shortname from course where course.id = $courseid");
+//                if (!$result) {
+//                    die('Invalid query' . mysql_error());
+//                }
+//                echo $result;
+            } else {        
+                 $courseid = $id->course;
+               create_quize_notification($courseid,$starttime); 
+//********************************** this was done to testing purpose****************************** at the real time nothing within else*?
+//           se['shortname'];
+            }
+            //  echo $result->fullname;     $courseid = $id->course;
+//               
+//                echo $resultcourse['shortname'];
+            }
+            //  echo $result->fullname;
+        }
+    
+ function create_quize_notification($courseid, $starttime){
+   
+    $con = mysql_connect("localhost", "root", "abc123");
+        if (!$con) {
+            die('Could not connect: ' . mysql_error());
+        }
+        mysql_select_db("moodle", $con);
+        
+     $result = mysql_query("select shortname from mdl_course where id = $courseid");
+                if (!$result) {
+                    die('Invalid query' . mysql_error());
+                }
+                $resultcourse = mysql_fetch_array($result);
+                echo $resultcourse['shortname'];
+                $datestamp = date("l jS \of F Y h:i:s A");
+               
+              
+                echo 'There is a quiz in the course';
+                echo $courseid ;
+                echo 'at';
+                echo $datestamp;
+                
+                
 }
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
