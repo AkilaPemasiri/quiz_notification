3af2b5f7e0929d0a5ccdec7443e91e0660bc7c66
100374A <akilasbv@gmail.com>
2013-09-22 14:05:22 +0530

    commit 6:  version 2013091162 user data saved successfully
    user data saved successfully

--- block_quiz_notification.php
+++ block_quiz_notification.php
@@ -8,7 +8,7 @@ class block_quiz_notification extends block_base {
 
     public function get_content() {
         global $USER, $CFG, $DB;
-        $user = $USER->username; //username of the current user
+        $user = $USER->id; //username of the current user
         $prf = $CFG->prefix;  //perfix of the moodle tables
         if ($this->content !== null) {
             return $this->content;
@@ -25,7 +25,7 @@ class block_quiz_notification extends block_base {
         $this->content->text .= '<form id="form1" name="form1" method="post" action="">';
         $this->content->text .= '<table width="120" border="0"><tr>';
         $this->content->text .= '<td width= "120"><input type = "display" name= "adress_label" value="' . get_string('view', 'block_quiz_notification') . '" readonly align="left"/></td></tr>';
-        $this->content->text .= '<tr><td width= "120"><input type = "display" name= "id_val" value="' . get_string('view1', 'block_quiz_notification') . '"a align="left"/></td>';
+        $this->content->text .= '<tr><td width= "120"><input type = "text" name= "id_val" value="' . get_string('view1', 'block_quiz_notification') . '"a align="left"/></td>';
         $this->content->text .= ' </tr></table>';
 
         //     $this->content->text .=      </table>';
@@ -43,20 +43,20 @@ class block_quiz_notification extends block_base {
             $userid = $USER->id;
             if ($DB->record_exists('quiz_notification_subs', array('id' => $userid))) { //Check whether the user has already subscribed
                 $this->content->text .= get_string('already_subscribed', 'block_quiz_notification');
-            }
-            else{
-                $facebook_id = $_POST[id_val];
-                if ($facebook_id =! "Enter the ID here"){
-                    
-                     $this->quiz_notification_subscribe($userid, $facebook_id);
+            } else {
+                $facebook_id = $_POST['id_val'];
+                // echo $facebook_id;
+                if (($facebook_id != "Enter the ID here") && ($facebook_id != null)) {
+
+                    $this->quiz_notification_subscribe($userid, $facebook_id);
+                    $this->content->text .= get_string('Enter_ID1', 'block_quiz_notification');
+                } else {
+
+                    $this->content->text .= get_string('Enter_ID', 'block_quiz_notification');
                 }
-               else{
-                  $this->content->text .= get_string('Enter ID', 'block_quiz_notification'); 
-               }
-                
             }
         }
-        ///************************************************************///
+///************************************************************///
 ///************************************************************///
 ///************************************************************///
     }
@@ -81,21 +81,21 @@ class block_quiz_notification extends block_base {
 
 
             //testing
-        
-        $starttime1 = $id->timeopen;
-        $starttime = $starttime1 - 19800; // to adjust the system time
-        $delay = $starttime - $now;
 
-        $gap = $delay / 60;
-        if (0 <= $gap && $gap <= 1) {
-            $courseid = $id->course;
-            create_quize_notification($courseid, $starttime);
-        } else {
-            $courseid = $id->course;
-            create_quize_notification($courseid, $starttime);
+            $starttime1 = $id->timeopen;
+            $starttime = $starttime1 - 19800; // to adjust the system time
+            $delay = $starttime - $now;
+
+            $gap = $delay / 60;
+            if (0 <= $gap && $gap <= 1) {
+                $courseid = $id->course;
+                create_quize_notification($courseid, $starttime);
+            } else {
+                $courseid = $id->course;
+                create_quize_notification($courseid, $starttime);
 //********************************** this was done to testing purpose****************************** at the real time nothing within else*?
 //           se['shortname'];
-        }
+            }
         }
         //  echo $result->fullname;     $courseid = $id->course;
 //               
@@ -103,31 +103,31 @@ class block_quiz_notification extends block_base {
     }
 
     //  echo $result->fullname;
-}
-
-function create_quize_notification($courseid, $starttime) {
-
-    $con = mysql_connect("localhost", "root", "abc123");
-    if (!$con) {
-        die('Could not connect: ' . mysql_error());
-    }
-    mysql_select_db("moodle", $con);
-
-    $result = mysql_query("select shortname from mdl_course where id = $courseid");
-    if (!$result) {
-        die('Invalid query' . mysql_error());
-    }
-    $resultcourse = mysql_fetch_array($result);
-    
-    $datestamp = date("l jS \of F Y h:i:s A");
 
 
-    $message = 'There is a quiz in the course ';
-    echo $message;
-   echo $resultcourse['shortname'];
-    echo ' at';
-    echo $datestamp;
-  //  echo $message;
+    function create_quize_notification($courseid, $starttime) {
+
+        $con = mysql_connect("localhost", "root", "abc123");
+        if (!$con) {
+            die('Could not connect: ' . mysql_error());
+        }
+        mysql_select_db("moodle", $con);
+
+        $result = mysql_query("select shortname from mdl_course where id = $courseid");
+        if (!$result) {
+            die('Invalid query' . mysql_error());
+        }
+        $resultcourse = mysql_fetch_array($result);
+
+        $datestamp = date("l jS \of F Y h:i:s A");
+
+
+        $message = 'There is a quiz in the course ';
+        echo $message;
+        echo $resultcourse['shortname'];
+        echo ' at';
+        echo $datestamp;
+        //  echo $message;
 //                echo $courseid ;
 //                echo 'at';
 //                echo $datestamp;
@@ -147,12 +147,25 @@ function create_quize_notification($courseid, $starttime) {
 //
 //    echo $result;
 //    echo 'End of jason';
-}
+    }
+
 //*this function is not tested**//
-function quiz_notification_subscribe($userid, $facebook_id){
-    
-    
+    function quiz_notification_subscribe($userid, $facebook_id) {
+        global $DB;
+        $table = 'quiz_notification_subs';
+        if ($DB->record_exists($table, array('user_id' => $userid, 'facebook_id' => $facebook_id))) { // if is not working 
+       
+            return TRUE;
+        }
+
+        $sub = new stdClass();
+        $sub->user_id = $userid;
+        $sub->facebook_id = $facebook_id;
+        $result = $DB->insert_record('quiz_notification_subs', $sub);
+    }
+
 }
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
--- db/upgrade.php
+++ db/upgrade.php
@@ -1,7 +1,78 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file keeps track of upgrades to the live module
+ *
+ * Sometimes, changes between versions involve alterations to database
+ * structures and other major things that may break installations. The upgrade
+ * function in this file will attempt to perform all the necessary actions to
+ * upgrade your older installation to the current version. If there's something
+ * it cannot do itself, it will tell you what you need to do.  The commands in
+ * here will all be database-neutral, using the functions defined in DLL libraries.
+ *
+ * @package    mod_live
+ * @copyright  2013 Sameera Nilupul
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-?>
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Execute live upgrade from the given old version
+ *
+ * @param int $oldversion
+ * @return bool
+ */
+function xmldb_block_quiz_notification_upgrade($oldversion) {
+    global $DB;
+    
+    $dbman = $DB->get_manager(); 
+    
+    if ($oldversion < 2013091154) {
+
+        
+        // Define field id to be added to quiz_notification_subs
+        $table = new xmldb_table('quiz_notification_subs');
+        $field = new xmldb_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
+
+        // Conditionally launch add field id
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        
+         // Define field user_id to be added to quiz_notification_subs
+        $table = new xmldb_table('quiz_notification_subs');
+        $field = new xmldb_field('user_id', XMLDB_TYPE_TEXT, null, null, null, null, null, 'id');
+
+        // Conditionally launch add field user_id
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        // Define field facebook_id to be added to quiz_notification_subs
+        $table = new xmldb_table('quiz_notification_subs');
+        $field = new xmldb_field('facebook_id', XMLDB_TYPE_TEXT, null, null, null, null, null, 'user_id');
+
+        // Conditionally launch add field facebook_id
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        // Another save point reached
+        upgrade_block_savepoint(true, 2013091154, 'quiz_notification');
+    }
+
+    return true;
+}
